d6dc07cb9e5db8ac0831942d04a3bea49c4662f1
pyrox18 <haryzizzudin@Haryzs-iMac.local>
2016-09-15 20:31:03 +0800

    Fix broken fuel data file

--- data/fuelPriceData.js
+++ data/fuelPriceData.js
@@ -1,6 +1,3 @@
-// JSON file containing data for the price of fuel types per litre.
-// Needs to be updated at the start of each month to reflect current prices.
-
 {
   "RON95": 1.70,
   "RON97": 2.05,
