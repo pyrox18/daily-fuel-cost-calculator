c55cda98e5ec799f89cba7709c24479af7cc5f68
pyrox18 <haryzizzudin@gmail.com>
2016-09-24 18:54:19 +0800

    Add reset all values button

--- controllers/advancedModeController.js
+++ controllers/advancedModeController.js
@@ -206,4 +206,18 @@ app.controller("advancedModeController", function($scope, $http) {
     $scope.addButtonDisable[index] = false;
   }
 
+  $scope.resetAllValues = function ()
+  {
+    $scope.bankBalance = 0;
+    for (var i = 0; i < $scope.fuelTypeData.length; i++)
+    {
+      $scope.fuelTypeData[i].commission = 0;
+      $scope.fuelTypeData[i].tankCapacity = 27000;
+      $scope.fuelTypeData[i].tankUllage = 10;
+      $scope.fuelTypeData[i].tankBalance = 0;
+      $scope.fuelTypeData[i].estSales = 0;
+      $scope.resetDay(i);
+    }
+  }
+
 });
