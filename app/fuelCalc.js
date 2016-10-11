caf37ed1c1ef7d0fa556c71a7cc1a0b8161cb426
pyrox18 <haryzizzudin@gmail.com>
2016-10-11 17:44:23 +0800

    Write floatsOnly input limiter directive

--- app/fuelCalc.js
+++ app/fuelCalc.js
@@ -1,6 +1,6 @@
-// AngularJS app initialisation and config for page routing
+// AngularJS app initialisation, routing config and directives
 
-var app = angular.module("fuelCalc", ['ngRoute']); 
+var app = angular.module("fuelCalc", ['ngRoute']);
 
 app.config(function($routeProvider) {
   $routeProvider
@@ -21,4 +21,36 @@ app.config(function($routeProvider) {
       templateUrl: 'modepages/advancedmode.html',
       controller: 'advancedModeController'
     })
+});
+
+// This directive still doesn't prevent 'e'/'+'/'-' as inputs
+app.directive('floatsOnly', function() {
+  return {
+    restrict: "A",
+    require: 'ngModel',
+    link: function(scope, element, attrs, modelCtrl) {
+      modelCtrl.$parsers.push(function (inputValue) {
+        if (inputValue == null) {
+          // Change empty number fields to always have a 0
+          inputValue = 0;
+          modelCtrl.$setViewValue(0);
+          modelCtrl.$render();
+        }
+        // Replace non-digit and non-decimal characters with ''
+        var transformedInput = inputValue.toString().replace(/[^\d\.]/g, '');
+        if (transformedInput != inputValue) {
+          modelCtrl.$setViewValue(transformedInput);
+          modelCtrl.$render();
+        }
+        // Remove leading 0 if input >= 1
+        if (modelCtrl.$viewValue >= 1 && modelCtrl.$viewValue[0] == 0) {
+          var newStr = modelCtrl.$viewValue.slice(1);
+          modelCtrl.$setViewValue(newStr);
+          modelCtrl.$render();
+        }
+
+        return transformedInput;
+      });
+    }
+  };
 });
