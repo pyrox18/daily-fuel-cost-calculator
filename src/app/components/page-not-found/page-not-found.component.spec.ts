6f2734c3ee90165e1f84a5349ba4607a1a6cf552
pyrox18 <haryzizzudin@gmail.com>
2017-04-21 15:19:20 +0800

    Implement page not found component

--- src/app/components/page-not-found/page-not-found.component.spec.ts
+++ src/app/components/page-not-found/page-not-found.component.spec.ts
@@ -1,10 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 import { PageNotFoundComponent } from './page-not-found.component';
 
 describe('PageNotFoundComponent', () => {
   let component: PageNotFoundComponent;
   let fixture: ComponentFixture<PageNotFoundComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -21,5 +25,11 @@ describe('PageNotFoundComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show message', () => {
+    de = fixture.debugElement.query(By.css('h3'));
+    el = de.nativeElement;
+    expect(el.textContent).toBe('Page not found. Please try going back.');
   });
 });
