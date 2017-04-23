32d835e446ab4814ec2828c37faa88c67017ee81
pyrox18 <haryzizzudin@gmail.com>
2017-04-23 20:01:27 +0800

    Implement standard mode

--- src/app/components/standard/standard.component.ts
+++ src/app/components/standard/standard.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { StandardFuel } from '../../models/standard-fuel';
+import { FuelPriceService } from '../../services/fuel-price.service';
+
 @Component({
   selector: 'app-standard',
   templateUrl: './standard.component.html',
@@ -7,9 +10,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StandardComponent implements OnInit {
 
-  constructor() { }
+  inputData: StandardFuel[];
+  bankBalance: number;
+  totalFuelCost: number;
+
+  constructor(
+    private fuelPriceService: FuelPriceService
+  ) { }
 
   ngOnInit() {
+    this.bankBalance = 0.00;
+    this.inputData = []
+    for (let i = 0; i < 3; i++) {
+      this.inputData.push(new StandardFuel());
+      this.inputData[i].fuelPrice = this.fuelPriceService.fuelPrices[i].price;
+    }
+    this.totalFuelCost = 0;
+  }
+
+  recalcTotalFuelCost() {
+    this.totalFuelCost = 0;
+    for (let data of this.inputData) {
+      this.totalFuelCost += data.totalFuelCost();
+    }
+  }
+
+  addDay(i: number) {
+    this.inputData[i].addDay();
+  }
+
+  removeDay(i: number) {
+    this.inputData[i].removeDay();
+    this.recalcTotalFuelCost();
+  }
+
+  resetFuelData(i: number) {
+    let fuelPrice = this.inputData[i].fuelPrice;
+    this.inputData[i].resetValues();
+    this.inputData[i].fuelPrice = fuelPrice;
+    this.recalcTotalFuelCost();
+  }
+
+  onFuelDataChange(index: number) {
+    this.inputData[index].addUpFuelReceiving()
+    this.inputData = this.inputData.slice();
+    this.recalcTotalFuelCost();
+  }
+
+  trackByFn(index: any, item: any) {
+    return index;
   }
 
 }
