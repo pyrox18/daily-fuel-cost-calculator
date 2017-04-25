c250026101592aa5bd502905a8c6541ca76d4bc8
pyrox18 <haryzizzudin@gmail.com>
2017-04-25 23:26:42 +0800

    Write bank balance component test

--- src/app/components/bank-balance/bank-balance.component.spec.ts
+++ src/app/components/bank-balance/bank-balance.component.spec.ts
@@ -5,6 +5,8 @@ import { BankBalanceComponent } from './bank-balance.component';
 describe('BankBalanceComponent', () => {
   let component: BankBalanceComponent;
   let fixture: ComponentFixture<BankBalanceComponent>;
+  let expectedBankBalance: number;
+  let expectedTotalFuelCost: number;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +18,19 @@ describe('BankBalanceComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BankBalanceComponent);
     component = fixture.componentInstance;
+    expectedBankBalance = 100000;
+    expectedTotalFuelCost = 50000;
+    component.bankBalance = expectedBankBalance;
+    component.totalFuelCost = expectedTotalFuelCost;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component.finalBalance).toBeDefined();
+  });
+
+  it('should calculate final balance', () => {
+    expect(component.finalBalance).toBeCloseTo(50000, 2);
   });
 });
--- src/app/components/bank-balance/bank-balance.component.ts
+++ src/app/components/bank-balance/bank-balance.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnChanges, Input } from '@angular/core';
+import { Component, OnChanges, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-bank-balance',
   templateUrl: './bank-balance.component.html',
   styleUrls: ['./bank-balance.component.css']
 })
-export class BankBalanceComponent implements OnChanges {
+export class BankBalanceComponent implements OnInit, OnChanges {
 
   @Input() bankBalance: number;
   @Input() totalFuelCost: number;
   finalBalance: number;
 
   constructor() { }
+
+  ngOnInit() {
+    if (this.bankBalance && this.totalFuelCost) {
+      this.finalBalance = this.bankBalance - this.totalFuelCost;  
+    }
+    else {
+      this.finalBalance = 0;
+    }
+  }
 
   ngOnChanges() {
     this.finalBalance = this.bankBalance - this.totalFuelCost;
