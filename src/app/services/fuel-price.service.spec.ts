e2b16426a961519d710b17bedca401743a8f5e95
pyrox18 <haryzizzudin@gmail.com>
2017-04-28 18:12:14 +0800

    Fix tests related to fuel-price service

--- src/app/services/fuel-price.service.spec.ts
+++ src/app/services/fuel-price.service.spec.ts
@@ -1,17 +1,38 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { Http } from '@angular/http';
 
 import { FuelPriceService } from './fuel-price.service';
 
 describe('FuelPriceService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [FuelPriceService]
-    });
-  });
+  let service: FuelPriceService;
 
-  it('should create service and possess fuel prices', inject([FuelPriceService], (service: FuelPriceService) => {
+  it('should create service and possess fuel prices', () => {
+    const fakeHttp = {};
+    service = new FuelPriceService(fakeHttp as Http);
+    const spy = spyOn(service, 'getFuelPrices').and.callFake(() => {
+      service.fuelPrices = [
+        {
+          "name": "RON95",
+          "price": 2.20
+        },
+        {
+          "name": "RON97",
+          "price": 2.50
+        },
+        {
+          "name": "Diesel",
+          "price": 2.05
+        },
+        {
+          "name": "Euro 5 Diesel",
+          "price": 2.15
+        }
+      ]
+    });
+
+    service.getFuelPrices();
     expect(service).toBeTruthy();
     expect(service.fuelPrices[0].name).toBe("RON95");
     expect(service.fuelPrices[0].price).toBeTruthy();
-  }));
+  });
 });
--- src/app/testing/service-stubs.ts
+++ src/app/testing/service-stubs.ts
@@ -1,4 +1,7 @@
-export let fuelPriceServiceStub = {
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+
+export const fuelPriceServiceStub = {
 
   fuelPrices: [
     {
@@ -17,6 +20,29 @@ export let fuelPriceServiceStub = {
       name: "Euro 5 Diesel",
       price: 2.15
     }
-  ]
+  ],
+  
+  getFuelPrices: () => {
+    return Observable.of(testFuelPrices);
+  }
 
 };
+
+const testFuelPrices = [
+  {
+    name: "RON95",
+    price: 2.20
+  },
+  {
+    name: "RON97",
+    price: 2.50
+  },
+  {
+    name: "Diesel",
+    price: 2.05
+  },
+  {
+    name: "Euro 5 Diesel",
+    price: 2.15
+  }
+]
